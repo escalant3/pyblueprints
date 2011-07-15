169376f6f970985d9aae9b7f4f5aef7717fb540a
Diego Muñoz Escalante <escalant3@gmail.com>
2011-07-15 17:25:21 -0400

    Updated Pypi Information (0.3.2)

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ __credits__ = [
     "Javier de la Rosa, https://github.com/versae",
 ]
 __license__ = "GPL 3"
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 __email__ = "escalant3@gmail.com"
 __url__ = "https://github.com/escalant3/pyblueprints"
 __description__ = """A Python version of the Blueprints
